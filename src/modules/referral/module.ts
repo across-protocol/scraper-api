586702859b5003748e54d3067417b6114d9fcfea
amateima <89395931+amateima@users.noreply.github.com>
2022-07-13 00:57:22 +0300

    fix: make ACX price 0.1$ (#10)
    Signed-off-by: amateima <amatei@umaproject.org>

--- src/modules/referral/module.ts
+++ src/modules/referral/module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppConfigModule } from "../configuration/configuration.module";
 import { Deposit } from "../scraper/model/deposit.entity";
 import { ReferralController } from "./entry-points/http/controller";
 import { ReferralService } from "./services/service";
@@ -8,6 +9,6 @@ import { ReferralService } from "./services/service";
   controllers: [ReferralController],
   exports: [ReferralService],
   providers: [ReferralService],
-  imports: [TypeOrmModule.forFeature([Deposit])],
+  imports: [TypeOrmModule.forFeature([Deposit]), AppConfigModule],
 })
 export class ReferralModule {}
