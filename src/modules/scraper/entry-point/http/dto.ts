f58c3d316ddeb3152541fb9544e3f0ae118a12e8
amateima <89395931+amateima@users.noreply.github.com>
2022-07-18 23:28:37 +0300

    fix: fix bug on fetching token prices (#14)

--- src/modules/scraper/entry-point/http/dto.ts
+++ src/modules/scraper/entry-point/http/dto.ts
@@ -15,6 +15,16 @@ export class ProcessBlocksBody {
   to: number;
 }
 
+export class ProcessPricesBody {
+  @IsInt()
+  @ApiProperty({ example: 1 })
+  fromDepositId: number;
+
+  @IsInt()
+  @ApiProperty({ example: 2 })
+  toDepositId: number;
+}
+
 export class SubmitReferralAddressJobBody {
   @IsInt()
   @ApiProperty({ example: 1 })
