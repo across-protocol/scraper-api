6f92619e3ef0b5c4625f57bd857587e6ddb84832
amateima <89395931+amateima@users.noreply.github.com>
2022-08-01 12:15:46 +0300

    chore: disable the materialized view's refresh (#31)
    * chore: disable the materialized view's refresh
    
    * Add env var

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -60,5 +60,6 @@ export default registerAs("config", () => {
     },
     acxUsdPrice: 0.1,
     enableSpokePoolsEventsProcessing: process.env.ENABLE_SPOKE_POOLS_EVENTS_PROCESSING === "true",
+    enableReferralsMaterializedViewRefresh: process.env.ENABLE_REFERRALS_MATERIALIZED_VIEW_REFRESH === "true",
   };
 });
