0a74208036f716119070db501a2038a58840b418
amateima <89395931+amateima@users.noreply.github.com>
2022-08-12 19:10:09 +0300

    fix: hot fix migration files (#47)

--- migrations/1659106715330-Deposit.ts
+++ migrations/1659106715330-Deposit.ts
@@ -1,10 +1,39 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class MaterializedView1659106715328 implements MigrationInterface {
-  name = "MaterializedView1659106715328";
+export class Deposit1659106715329 implements MigrationInterface {
+  name = "Deposit1659106715329";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`drop materialized view if exists deposits_mv`);
+
+    await queryRunner.query(`DROP VIEW IF EXISTS "deposit_referral_stats";`);
+
+    await queryRunner.query(`
+      CREATE VIEW "deposit_referral_stats" as
+      (
+        select
+          d1.id,
+          count(distinct d2."depositorAddr") as "referralCount",
+          sum((d2.amount / power(10, t.decimals)) * hmp.usd) as "referralVolume"
+        from deposit d1
+        left join deposit d2 on d1."stickyReferralAddress" = d2."stickyReferralAddress"
+          and d1."depositDate" >= d2."depositDate"
+          and d1."stickyReferralAddress" is not null
+          and d1."depositDate" is not null
+          and d1."tokenId" is not null
+          and d1."priceId" is not null
+          and d1.status = 'filled'
+        join historic_market_price hmp on d2."priceId" = hmp.id
+        join token t on d2."tokenId" = t.id
+        where d1."stickyReferralAddress" is not null
+          and d1."depositDate" is not null
+          and d1."tokenId" is not null
+          and d1."priceId" is not null
+          and d1.status = 'filled'
+        group by d1.id
+      );
+    `);
+
     await queryRunner.query(`
       create materialized view deposits_mv as
         select
@@ -20,6 +49,7 @@ export class MaterializedView1659106715328 implements MigrationInterface {
           d."depositDate",
           hmp.usd as "tokenUsdPrice",
           (d."realizedLpFeePctCapped" / power(10, 18)) * (d.amount / power(10, t.decimals)) * hmp.usd as "realizedLpFeeUsd",
+          (d."bridgeFeePct" / power(10, 18)) * (d.amount / power(10, t.decimals)) * hmp.usd as "bridgeFeeUsd",
         case
           when d1."referralCount" >= 20 or d1."referralVolume" >= 500000 then 0.8
           when d1."referralCount" >= 10 or d1."referralVolume" >= 250000 then 0.7
@@ -37,6 +67,7 @@ export class MaterializedView1659106715328 implements MigrationInterface {
         join historic_market_price hmp on d."priceId" = hmp.id
         order by d."depositDate" desc;
     `);
+
     await queryRunner.query(
       `CREATE UNIQUE INDEX "UK_deposits_mv_depositId_sourceChainId" ON deposits_mv ("depositId", "sourceChainId");`,
     );
@@ -44,5 +75,6 @@ export class MaterializedView1659106715328 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`drop materialized view if exists deposits_mv`);
+    await queryRunner.query(`DROP VIEW IF EXISTS "deposit_referral_stats";`);
   }
 }
