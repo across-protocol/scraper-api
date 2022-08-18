d5ecc701ec272fbcd67494118ede3d5e04d66016
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-08-18 16:03:03 +0200

    test: e2e for deposit module

--- src/modules/scraper/adapter/db/deposit-fixture.ts
+++ src/modules/scraper/adapter/db/deposit-fixture.ts
@@ -1,45 +1,67 @@
+import { DateTime, Duration } from "luxon";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { Deposit } from "../../model/deposit.entity";
+import { Deposit, DepositFillTx, TransferStatus } from "../../model/deposit.entity";
 
 @Injectable()
 export class DepositFixture {
   public constructor(@InjectRepository(Deposit) private depositRepository: Repository<Deposit>) {}
 
-  public insertDeposit({
-    depositId,
-    destinationChainId,
-    sourceChainId,
-    depositorAddr = "0x",
-    amount = "0",
-    tokenAddr = "0x",
-    depositTxHash = "0x",
-    blockNumber = 1,
-  }: {
-    depositId: number;
-    sourceChainId: number;
-    destinationChainId: number;
-    depositorAddr?: string;
-    amount?: string;
-    tokenAddr?: string;
-    depositTxHash?: string;
-    blockNumber?: number;
-  }) {
-    const deposit = this.depositRepository.create({
-      depositId,
-      sourceChainId,
-      destinationChainId,
-      depositorAddr,
-      amount,
-      tokenAddr,
-      depositTxHash,
-      blockNumber,
-    });
+  public insertDeposit(depositArgs: Partial<Deposit>) {
+    const deposit = this.depositRepository.create(mockDepositEntity(depositArgs));
     return this.depositRepository.save(deposit);
+  }
+
+  public insertManyDeposits(deposits: Partial<Deposit>[]) {
+    const createdDeposits = this.depositRepository.create(deposits);
+    return this.depositRepository.save(createdDeposits);
   }
 
   public deleteAllDeposits() {
     return this.depositRepository.query(`truncate table "deposit" restart identity cascade`);
   }
+}
+
+export function mockDepositEntity(overrides: Partial<Deposit>) {
+  return {
+    depositId: 1,
+    sourceChainId: 1,
+    destinationChainId: 1,
+    depositDate: new Date(),
+    depositorAddr: "0x",
+    status: "pending" as TransferStatus,
+    amount: "0",
+    filled: "0",
+    realizedLpFeePct: "0",
+    realizedLpFeePctCapped: "0",
+    bridgeFeePct: "0",
+    tokenAddr: "0x",
+    depositTxHash: "0x",
+    fillTxs: [] as DepositFillTx[],
+    blockNumber: 1,
+    ...overrides,
+  };
+}
+
+export function mockManyDepositEntities(
+  n: number,
+  options: Partial<{
+    depositIdStartIndex: number;
+    overrides: Partial<Deposit>;
+  }>,
+) {
+  const { depositIdStartIndex = 1, overrides } = options;
+
+  return Array(n)
+    .fill(0)
+    .map((zero, i) =>
+      mockDepositEntity({
+        depositId: depositIdStartIndex + i,
+        depositDate: DateTime.now()
+          .minus(Duration.fromObject({ days: n - i }))
+          .toJSDate(),
+        ...overrides,
+      }),
+    );
 }
---
+++ test/deposit.e2e-spec.ts
@@ -0,0 +1,76 @@
+import request from "supertest";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { DepositFixture, mockManyDepositEntities } from "../src/modules/scraper/adapter/db/deposit-fixture";
+import { AppModule } from "../src/app.module";
+
+describe("Deposit module", () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  describe("GET /deposits", () => {
+    // filled deposits with depositIds 10 - 19 and ascending depositDate
+    const FILLED_DEPOSITS = mockManyDepositEntities(10, { depositIdStartIndex: 10, overrides: { status: "filled" } });
+    // pending deposits with depositIds 20 - 29 and ascending depositDate
+    const PENDING_DEPOSITS = mockManyDepositEntities(10, { depositIdStartIndex: 20, overrides: { status: "pending" } });
+
+    beforeAll(async () => {
+      await app.get(DepositFixture).insertManyDeposits([...FILLED_DEPOSITS, ...PENDING_DEPOSITS]);
+    });
+
+    it("200 without params", async () => {
+      const response = await request(app.getHttpServer()).get("/deposits");
+      expect(response.status).toBe(200);
+      expect(response.body.deposits).toHaveLength(10);
+      expect(response.body.pagination).toMatchObject({ limit: 10, offset: 0 });
+    });
+
+    it("200 with status=filled & limit=5 ", async () => {
+      const response = await request(app.getHttpServer()).get("/deposits?status=filled&limit=5");
+      expect(response.status).toBe(200);
+      expect(response.body.deposits).toHaveLength(5);
+      expect(response.body.pagination).toMatchObject({ limit: 5, offset: 0, total: FILLED_DEPOSITS.length });
+      expect(response.body.deposits[0].depositId).toBe(19);
+    });
+
+    it("200 with status=filled & limit=5 & skip=5", async () => {
+      const response = await request(app.getHttpServer()).get("/deposits?status=filled&limit=5&offset=5");
+      expect(response.status).toBe(200);
+      expect(response.body.deposits).toHaveLength(5);
+      expect(response.body.pagination).toMatchObject({ limit: 5, offset: 5, total: FILLED_DEPOSITS.length });
+      expect(response.body.deposits[0].depositId).toBe(14);
+    });
+
+    it("400 for invalid status", async () => {
+      const response = await request(app.getHttpServer()).get("/deposits?status=invalid");
+      expect(response.status).toBe(400);
+    });
+
+    it("400 for invalid offset", async () => {
+      const response = await request(app.getHttpServer()).get("/deposits?offset=invalid");
+      expect(response.status).toBe(400);
+    });
+
+    it("400 for invalid limit", async () => {
+      const response = await request(app.getHttpServer()).get("/deposits?limit=invalid");
+      expect(response.status).toBe(400);
+    });
+
+    afterAll(async () => {
+      await app.get(DepositFixture).deleteAllDeposits();
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
