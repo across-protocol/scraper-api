806eb79e4478e5a4f73489a3cd0ddad51a581018
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-21 13:33:44 -0400

    fix: include jwt strategy

--- src/modules/user/module.ts
+++ src/modules/user/module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtStrategy } from "../auth/entry-points/http/jwt.strategy";
 import { UserController } from "./entry-points/http/user.controller";
 import { User } from "./model/user.entity";
 import { UserService } from "./services/user.service";
 
 @Module({
-  providers: [UserService],
+  providers: [JwtStrategy, UserService],
   controllers: [UserController],
   imports: [TypeOrmModule.forFeature([User])],
   exports: [UserService],
