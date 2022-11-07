14f9a3fecc4208f7d25f542946be350e1e8014ca
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-11-07 23:25:09 +0700

    refactor: split up migrations

--- src/modules/deposit/model/DepositsMv.entity.ts
+++ src/modules/deposit/model/DepositsMv.entity.ts
@@ -6,7 +6,7 @@ import { DepositReferralStats } from "../../referral/model/DepositReferralStats.
   dependsOn: [DepositReferralStats],
   materialized: true,
   expression: `
-    select
+    SELECT
       d."depositId",
       d."depositTxHash",
       d."sourceChainId",
@@ -15,27 +15,27 @@ import { DepositReferralStats } from "../../referral/model/DepositReferralStats.
       t.symbol,
       t.decimals,
       d."depositorAddr",
-      d."stickyReferralAddress" as "referralAddress",
+      d."stickyReferralAddress" AS "referralAddress",
       d."depositDate",
-      hmp.usd as "tokenUsdPrice",
-      (d."realizedLpFeePctCapped" / power(10, 18)) * (d.amount / power(10, t.decimals)) * hmp.usd as "realizedLpFeeUsd",
-      (d."bridgeFeePct" / power(10, 18)) * (d.amount / power(10, t.decimals)) * hmp.usd as "bridgeFeeUsd",
-    case
-      when d1."referralCount" >= 20 or d1."referralVolume" >= 500000 then 0.8
-      when d1."referralCount" >= 10 or d1."referralVolume" >= 250000 then 0.7
-      when d1."referralCount" >= 5 or d1."referralVolume" >= 100000 then 0.6
-      when d1."referralCount" >= 3 or d1."referralVolume" >= 50000 then 0.5
-      else 0.4
-    end as "referralRate",
-    case
-        when d."depositDate" < '2022-07-22 17:00:00' then 3
-        else 2
-    end as multiplier
-    from deposit d
-    join "deposit_referral_stats" d1 on d.id = d1.id
-    join token t on d."tokenId" = t.id
-    join historic_market_price hmp on d."priceId" = hmp.id
-    order by d."depositDate" desc;
+      hmp.usd AS "tokenUsdPrice",
+      (d."realizedLpFeePctCapped" / power(10, 18)) * (d.amount / power(10, t.decimals)) * hmp.usd AS "realizedLpFeeUsd",
+      (d."bridgeFeePct" / power(10, 18)) * (d.amount / power(10, t.decimals)) * hmp.usd AS "bridgeFeeUsd",
+    CASE
+      WHEN d1."referralCount" >= 20 OR d1."referralVolume" >= 500000 THEN 0.8
+      WHEN d1."referralCount" >= 10 OR d1."referralVolume" >= 250000 THEN 0.7
+      WHEN d1."referralCount" >= 5 OR d1."referralVolume" >= 100000 THEN 0.6
+      WHEN d1."referralCount" >= 3 OR d1."referralVolume" >= 50000 THEN 0.5
+      ELSE 0.4
+    END AS "referralRate",
+    CASE
+        WHEN d."depositDate" < '2022-07-22 17:00:00' THEN 3
+        ELSE 2
+    END AS multiplier
+    FROM deposit d
+    JOIN "deposit_referral_stats" d1 ON d.id = d1.id
+    JOIN token t ON d."tokenId" = t.id
+    JOIN historic_market_price hmp ON d."priceId" = hmp.id
+    ORDER BY d."depositDate" desc;
   `,
 })
 @Unique("UK_deposits_mv_depositId_sourceChainId", ["depositId", "sourceChainId"])
