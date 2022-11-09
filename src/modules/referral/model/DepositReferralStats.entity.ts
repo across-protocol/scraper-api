662a840255da3f10ca3738789c9f9d41ef111d61
amateima <amatei@umaproject.org>
2022-11-09 18:35:36 +0200

    Update referrals materialized view

--- src/modules/referral/model/DepositReferralStats.entity.ts
+++ src/modules/referral/model/DepositReferralStats.entity.ts
@@ -1,35 +1,21 @@
 import { ViewEntity, ViewColumn } from "typeorm";
-
-import { configValues } from "../../configuration";
+import { DepositsFilteredReferrals } from "./DepositsFilteredReferrals.entity";
 
 @ViewEntity({
   expression: `
-    SELECT d1.id,
+    SELECT 
+      d1.id,
       count(distinct d2."depositorAddr") as "referralCount",
       sum((d2.amount / power(10, t.decimals)) * hmp.usd) as "referralVolume"
-    FROM deposit d1
-    LEFT JOIN deposit d2 on d1."stickyReferralAddress" = d2."stickyReferralAddress"
-      AND d1."depositDate" >= d2."depositDate"
-      AND d1."stickyReferralAddress" is not null
-      AND d1."depositDate" is not null
-      AND d1."tokenId" is not null
-      AND d1."priceId" is not null
-      AND d1.status = 'filled'
-      AND d2."depositDate" >= (SELECT  coalesce(max(c."claimedAt"), '2000-01-01 00:00:00')
-                               FROM    claim c
-                               WHERE   c.account = d2."stickyReferralAddress"
-                               AND     c."windowIndex" >= ${
-                                 configValues().web3.merkleDistributor.referralsStartWindowIndex
-                               })
+    FROM deposits_filtered_referrals d1
+    LEFT JOIN deposits_filtered_referrals d2 on d1."stickyReferralAddress" = d2."stickyReferralAddress" AND 
+      d1."depositDate" >= d2."depositDate" AND 
+      d1."claimedWindowIndex" = d2."claimedWindowIndex"
     JOIN historic_market_price hmp on d2."priceId" = hmp.id
     JOIN token t on d2."tokenId" = t.id
-    WHERE d1."stickyReferralAddress" is not null
-      AND d1."depositDate" is not null
-      AND d1."tokenId" is not null
-      AND d1."priceId" is not null
-      AND d1.status = 'filled'
     GROUP BY d1.id
   `,
+  dependsOn: [DepositsFilteredReferrals],
 })
 export class DepositReferralStats {
   @ViewColumn()
---
+++ src/modules/referral/model/DepositsFilteredReferrals.entity.ts
@@ -0,0 +1,44 @@
+import { DepositFillTx, TransferStatus } from "src/modules/scraper/model/deposit.entity";
+import { ViewEntity, ViewColumn } from "typeorm";
+
+@ViewEntity({
+  expression: `
+    SELECT 
+      d.id,
+      d."stickyReferralAddress",
+      d."depositDate",
+      d."priceId",
+      d."tokenId",
+      d.amount,
+      case when d."rewardsWindowIndex" = c."windowIndex" then d."rewardsWindowIndex" else -1 end as "claimedWindowIndex"
+    FROM deposit d
+    left join claim c on d."rewardsWindowIndex" = c."windowIndex" and d."referralAddress" = c."account"
+    WHERE "stickyReferralAddress" is not null
+      AND "depositDate" is not null
+      AND "tokenId" is not null
+      AND "priceId" is not null
+      AND status = 'filled';
+  `,
+})
+export class DepositsFilteredReferrals {
+  @ViewColumn()
+  id: number;
+
+  @ViewColumn()
+  stickyReferralAddress?: string;
+
+  @ViewColumn()
+  tokenId?: number;
+
+  @ViewColumn()
+  priceId?: number;
+
+  @ViewColumn()
+  depositDate?: Date;
+
+  @ViewColumn()
+  amount: string;
+
+  @ViewColumn()
+  claimedWindowIndex?: number;
+}
