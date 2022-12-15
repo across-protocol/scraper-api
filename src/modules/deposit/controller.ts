4ba3c2352ef8a7ef38e13dd6758525a0e484a538
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-12-15 14:44:37 +0100

    fixup

--- src/modules/deposit/controller.ts
+++ src/modules/deposit/controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { DepositService } from "./service";
 import { GetDepositsQuery, GetDepositsStatsResponse } from "./dto";
