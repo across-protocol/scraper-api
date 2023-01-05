6e8c51d1c45b7664db5d3ec6a39ebc838a3c0261
amateima <amatei@umaproject.org>
2023-01-05 13:44:21 +0200

    Update deposit module

--- src/modules/deposit/module.ts
+++ src/modules/deposit/module.ts
@@ -6,21 +6,17 @@ import { Deposit } from "./model/deposit.entity";
 import { DepositController } from "./controller";
 import { DepositService } from "./service";
 
-@Module({
-  controllers: [DepositController],
-  exports: [DepositService],
-  providers: [DepositService],
-  imports: [TypeOrmModule.forFeature([Deposit]), AppConfigModule],
-})
+@Module({})
 export class DepositModule {
   static forRoot(moduleOptions: ModuleOptions): DynamicModule {
     let module: DynamicModule = { module: DepositModule, providers: [], controllers: [], imports: [], exports: [] };
 
-    if (moduleOptions.runModes.includes(RunMode.Normal)) {
+    if (moduleOptions.runModes.includes(RunMode.Normal) || moduleOptions.runModes.includes(RunMode.Test)) {
       module = {
         ...module,
         controllers: [...module.controllers, DepositController],
         providers: [...module.providers, DepositService],
+        exports: [...module.exports, DepositService],
         imports: [...module.imports, TypeOrmModule.forFeature([Deposit]), AppConfigModule],
       };
     }
