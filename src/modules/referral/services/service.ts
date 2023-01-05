23baf7045000727951f62aee63b84fb56dfe4a4e
amateima <amatei@umaproject.org>
2023-01-05 23:47:53 +0200

    fix: fix referral address extraction

--- src/modules/referral/services/service.ts
+++ src/modules/referral/services/service.ts
@@ -171,18 +171,29 @@ export class ReferralService {
   }
 
   public subtractFunctionArgsFromCallData(data: string) {
-    const coder = new ethers.utils.AbiCoder();
-    // strip hex method identifier
-    const dataNoMethod = ethers.utils.hexDataSlice(data, 4);
-    // keep method hex identifier
-    const methodHex = data.replace(dataNoMethod.replace("0x", ""), "");
-    const decodedData = coder.decode(
-      ["address", "address", "uint256", "uint256", "uint64", "uint32"],
-      ethers.utils.hexDataSlice(data, 4),
-    );
-    const encoded = coder.encode(["address", "address", "uint256", "uint256", "uint64", "uint32"], decodedData);
-    const fullEncoded = methodHex + encoded.replace("0x", "");
-    return data.replace(fullEncoded, "");
+    try {
+      const coder = new ethers.utils.AbiCoder();
+      // strip hex method identifier
+      const dataNoMethod = ethers.utils.hexDataSlice(data, 4);
+      // keep method hex identifier
+      const methodHex = data.replace(dataNoMethod.replace("0x", ""), "");
+      const decodedData = coder.decode(
+        ["address", "address", "uint256", "uint256", "uint64", "uint32"],
+        ethers.utils.hexDataSlice(data, 4),
+      );
+      const encoded = coder.encode(["address", "address", "uint256", "uint256", "uint64", "uint32"], decodedData);
+      const fullEncoded = methodHex + encoded.replace("0x", "");
+      return data.replace(fullEncoded, "");
+    } catch (error) {
+      console.log(JSON.stringify(error, undefined, 2));
+      if (error.reason === "value out of range" && error.code === "INVALID_ARGUMENT") {
+        return "";
+      }
+      if (error.reason === "data out-of-bounds" && error.code === "BUFFER_OVERRUN") {
+        return "";
+      }
+      throw error;
+    }
   }
 
   public extractReferralAddressUsingDelimiter(data: string) {
