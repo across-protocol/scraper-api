cd2b8a15ee4f3abea7b0ee8955f90b3ec08d1401
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-05-30 12:00:30 -0400

    fix: resolve block

--- src/modules/scraper/blocks.spec.ts
+++ src/modules/scraper/blocks.spec.ts
@@ -4,9 +4,9 @@ describe("blocks", () => {
   it("[10, 20, 30] 2 10", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       2,
       10,
@@ -14,144 +14,144 @@ describe("blocks", () => {
     expect(interval).toEqual(undefined);
   });
   it("[10] 10 12", () => {
-    const interval = splitBlockRanges([{ startBlockNumber: 10, address: "0x1" }], 10, 12);
-    expect(interval).toEqual([{ from: 10, to: 12, address: "0x1" }]);
+    const interval = splitBlockRanges([{ startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" }], 10, 12);
+    expect(interval).toEqual([{ from: 10, to: 12, address: "0x1", acrossVersion: "1.0" }]);
   });
   it("[10, 20, 30] 10 12", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       10,
       12,
     );
-    expect(interval).toEqual([{ from: 10, to: 12, address: "0x1" }]);
+    expect(interval).toEqual([{ from: 10, to: 12, address: "0x1", acrossVersion: "1.0" }]);
   });
   it("[10, 20, 30] 20 21", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       20,
       21,
     );
-    expect(interval).toEqual([{ from: 20, to: 21, address: "0x2" }]);
+    expect(interval).toEqual([{ from: 20, to: 21, address: "0x2", acrossVersion: "1.0" }]);
   });
   it("[10, 20, 30] 10 20", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       10,
       20,
     );
     expect(interval).toEqual([
-      { from: 10, to: 19, address: "0x1" },
-      { from: 20, to: 20, address: "0x2" },
+      { from: 10, to: 19, address: "0x1", acrossVersion: "1.0" },
+      { from: 20, to: 20, address: "0x2", acrossVersion: "1.0" },
     ]);
   });
   it("[10, 20, 30] 10 21", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       10,
       21,
     );
     expect(interval).toEqual([
-      { from: 10, to: 19, address: "0x1" },
-      { from: 20, to: 21, address: "0x2" },
+      { from: 10, to: 19, address: "0x1", acrossVersion: "1.0" },
+      { from: 20, to: 21, address: "0x2", acrossVersion: "1.0" },
     ]);
   });
   it("[10, 20, 30] 10 35", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       10,
       35,
     );
     expect(interval).toEqual([
-      { from: 10, to: 19, address: "0x1" },
-      { from: 20, to: 29, address: "0x2" },
-      { from: 30, to: 35, address: "0x3" },
+      { from: 10, to: 19, address: "0x1", acrossVersion: "1.0" },
+      { from: 20, to: 29, address: "0x2", acrossVersion: "1.0" },
+      { from: 30, to: 35, address: "0x3", acrossVersion: "1.0" },
     ]);
   });
   it("[10, 20, 30] 10 30", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       10,
       30,
     );
     expect(interval).toEqual([
-      { from: 10, to: 19, address: "0x1" },
-      { from: 20, to: 29, address: "0x2" },
-      { from: 30, to: 30, address: "0x3" },
+      { from: 10, to: 19, address: "0x1", acrossVersion: "1.0" },
+      { from: 20, to: 29, address: "0x2", acrossVersion: "1.0" },
+      { from: 30, to: 30, address: "0x3", acrossVersion: "1.0" },
     ]);
   });
   it("[5, 10, 20, 30] 10 30", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 5, address: "0x05" },
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 5, address: "0x05", acrossVersion: "1.0" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       10,
       30,
     );
     expect(interval).toEqual([
-      { from: 10, to: 19, address: "0x1" },
-      { from: 20, to: 29, address: "0x2" },
-      { from: 30, to: 30, address: "0x3" },
+      { from: 10, to: 19, address: "0x1", acrossVersion: "1.0" },
+      { from: 20, to: 29, address: "0x2", acrossVersion: "1.0" },
+      { from: 30, to: 30, address: "0x3", acrossVersion: "1.0" },
     ]);
   });
   it("[5, 10, 20, 30] 11 30", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 5, address: "0x05" },
-        { startBlockNumber: 10, address: "0x1" },
-        { startBlockNumber: 20, address: "0x2" },
-        { startBlockNumber: 30, address: "0x3" },
+        { startBlockNumber: 5, address: "0x05", acrossVersion: "1.0" },
+        { startBlockNumber: 10, address: "0x1", acrossVersion: "1.0" },
+        { startBlockNumber: 20, address: "0x2", acrossVersion: "1.0" },
+        { startBlockNumber: 30, address: "0x3", acrossVersion: "1.0" },
       ],
       11,
       30,
     );
     expect(interval).toEqual([
-      { from: 11, to: 19, address: "0x1" },
-      { from: 20, to: 29, address: "0x2" },
-      { from: 30, to: 30, address: "0x3" },
+      { from: 11, to: 19, address: "0x1", acrossVersion: "1.0" },
+      { from: 20, to: 29, address: "0x2", acrossVersion: "1.0" },
+      { from: 30, to: 30, address: "0x3", acrossVersion: "1.0" },
     ]);
   });
   it("[0, 3, 7, 9] 2 8", () => {
     const interval = splitBlockRanges(
       [
-        { startBlockNumber: 0, address: "0x0" },
-        { startBlockNumber: 3, address: "0x3" },
-        { startBlockNumber: 7, address: "0x7" },
-        { startBlockNumber: 9, address: "0x9" },
+        { startBlockNumber: 0, address: "0x0", acrossVersion: "1.0" },
+        { startBlockNumber: 3, address: "0x3", acrossVersion: "1.0" },
+        { startBlockNumber: 7, address: "0x7", acrossVersion: "1.0" },
+        { startBlockNumber: 9, address: "0x9", acrossVersion: "1.0" },
       ],
       2,
       8,
     );
     expect(interval).toEqual([
-      { from: 2, to: 2, address: "0x0" },
-      { from: 3, to: 6, address: "0x3" },
-      { from: 7, to: 8, address: "0x7" },
+      { from: 2, to: 2, address: "0x0", acrossVersion: "1.0" },
+      { from: 3, to: 6, address: "0x3", acrossVersion: "1.0" },
+      { from: 7, to: 8, address: "0x7", acrossVersion: "1.0" },
     ]);
   });
 });
