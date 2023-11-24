03a73efd9db6e5f396b9ea8a8a42618a29f92374
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-11-24 17:45:12 +0100

    fix: migration file

--- migrations/1700844010607-Reward.ts
+++ migrations/1700844010607-Reward.ts
@@ -8,6 +8,6 @@ export class Reward1700844010607 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "deposit" ALTER COLUMN "depositRelayerFeePct" DROP NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "reward" DROP COLUMN "depositDate"`);
   }
 }
