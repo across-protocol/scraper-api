e30577345bb9780dd4e16cf143e2b9879164a903
amateima <amateima@umaproject.org>
2024-07-05 22:26:07 +0300

    Fix build

--- src/modules/referral/module.ts
+++ src/modules/referral/module.ts
@@ -4,7 +4,6 @@ import { AppConfigModule } from "../configuration/configuration.module";
 import { DepositsMv } from "../deposit/model/DepositsMv.entity";
 import { Deposit } from "../deposit/model/deposit.entity";
 import { ReferralController } from "./entry-points/http/controller";
-import { ReferralCronService } from "./services/cron-service";
 import { ReferralService } from "./services/service";
 import { ModuleOptions, RunMode } from "../../dynamic-module";
 import { Web3Module } from "../web3/module";
@@ -42,7 +41,7 @@ export class ReferralModule {
     if (moduleOptions.runModes.includes(RunMode.Scraper)) {
       module = {
         ...module,
-        providers: [...module.providers, ReferralCronService, ReferralService],
+        providers: [...module.providers, ReferralService],
         imports: [
           ...module.imports,
           TypeOrmModule.forFeature([
