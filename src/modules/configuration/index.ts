0533483d39953ce510e841fd1af8c0a68385dcfa
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-10-08 17:46:16 +0200

    fiixup

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -397,7 +397,7 @@ export const configValues = () => ({
           firstDepositId: 1,
         },
       ],
-      [ChainIds.zora]: [
+      [ChainIds.worldChain]: [
         {
           address: "0x09aea4b2242abC8bb4BB78D537A67a245A7bEC64",
           startBlockNumber: 4524742,
