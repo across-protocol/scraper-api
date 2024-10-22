bb0375e22807d13861925d7054a8b57a9332b8ca
amateima <89395931+amateima@users.noreply.github.com>
2024-10-22 15:47:49 +0300

    feat: add pool rebalance route date (#494)
    Co-authored-by: amateima <amateima@umaproject.org>

--- src/modules/scraper/adapter/messaging/HubPoolBlocksEventsConsumer.ts
+++ src/modules/scraper/adapter/messaging/HubPoolBlocksEventsConsumer.ts
@@ -8,6 +8,8 @@ import { HubPoolBlocksEventsQueueMessage, ScraperQueue } from ".";
 import { AppConfig } from "../../../configuration/configuration.service";
 import { SetPoolRebalanceRoute } from "../../../web3/model/hubpool-events";
 import { SetPoolRebalanceRouteEvent } from "../../../web3/model/SetPoolRebalanceRouteEvent.entity";
+import { ChainIds } from "../../../web3/model/ChainId";
+import { Block } from "../../../web3/model/block.entity";
 
 @Processor(ScraperQueue.HubPoolBlocksEvents)
 export class HubPoolBlocksEventsConsumer {
@@ -27,6 +29,12 @@ export class HubPoolBlocksEventsConsumer {
       .getHubPoolEventQuerier(chainId, address)
       .getSetPoolRebalanceRouteEvents(from, to)) as SetPoolRebalanceRoute[];
     this.logger.log(`(${from}, ${to}) - chainId ${chainId} - found ${setPoolRebalanceRouteEvents.length} SetPoolRebalanceRouteEvent`);
+    const blocksByNumber: Record<number, Block> = {};
+
+    for (const event of setPoolRebalanceRouteEvents) {
+      const block = await this.providers.getCachedBlock(ChainIds.mainnet, event.blockNumber);
+      blocksByNumber[event.blockNumber] = block;
+    }
 
     if (setPoolRebalanceRouteEvents.length > 0) {
       for (const event of setPoolRebalanceRouteEvents) {
@@ -35,6 +43,7 @@ export class HubPoolBlocksEventsConsumer {
           .insert()
           .into(SetPoolRebalanceRouteEvent)
           .values({
+            date: blocksByNumber[event.blockNumber].date,
             blockNumber: event.blockNumber,
             blockHash: event.blockHash,
             transactionIndex: event.transactionIndex,
--- src/modules/web3/model/SetPoolRebalanceRouteEvent.entity.ts
+++ src/modules/web3/model/SetPoolRebalanceRouteEvent.entity.ts
@@ -18,6 +18,9 @@ export class SetPoolRebalanceRouteEvent {
   transactionIndex: number;
 
   @Column()
+  date: Date;
+
+  @Column()
   address: string;
 
   @Column()
