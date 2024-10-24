2b9d62d451d154cca1053e1fdc3d1faa40416adc
Melisa Guevara <melisaguevarav@gmail.com>
2024-10-24 09:56:54 -0300

    feat: add lisk to op rewards (#496)
    Add Lisk to OP rebates

--- src/modules/rewards/services/op-rebate-service.ts
+++ src/modules/rewards/services/op-rebate-service.ts
@@ -21,11 +21,12 @@ const OP_REBATE_RATE = 0.95;
 const REWARDS_PERCENTAGE_LIMIT = 0.0025; // 25 bps
 const ELIGIBLE_OP_REWARDS_CHAIN_IDS = [
   ChainIds.base,
+  ChainIds.lisk,
   ChainIds.mode,
   ChainIds.optimism,
-  ChainIds.zora,
   ChainIds.redstone,
   ChainIds.worldChain,
+  ChainIds.zora,
 ];
 
 const partialDepositKeys: (keyof Deposit)[] = [
