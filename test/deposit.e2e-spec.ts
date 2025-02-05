763554e18b083f8378d175420cce932fbf257908
amateima <amateima@umaproject.org>
2025-02-05 11:58:09 +0200

    Fix lint

--- test/deposit.e2e-spec.ts
+++ test/deposit.e2e-spec.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
-import { constants, ethers } from "ethers";
+import { constants } from "ethers";
 import { DateTime } from "luxon";
 
 import {
